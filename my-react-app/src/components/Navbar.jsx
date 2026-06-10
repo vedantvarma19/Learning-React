dcf66b14b9793b0c2ad0ab43c4c7e67afd32018a
Vedant Varma <vedantvarma617@gmail.com>
2026-06-10 06:58:17 +0530

    Day 1: Set up React project with Vite

---
+++ my-react-app/src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+export default function Navbar() {
+  return (
+    <header>
+      <nav>
+        <img src="/React.png" alt="React Logo" />
+        <span>React Facts</span>
+      </nav>
+    </header>
+  );
+}
